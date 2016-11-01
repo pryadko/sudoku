f4cbf33fa1251e4cd397c28b01b517c80d58bd72
alex <pryadko@gmail.com>
2016-11-02 00:19:54 +0200

    implement load board from file

--- src/main/java/com/pryadko/process/Loader.java
+++ src/main/java/com/pryadko/process/Loader.java
@@ -3,11 +3,32 @@ package com.pryadko.process;
 import com.pryadko.domain.Board;
 import org.springframework.stereotype.Service;
 
+import java.io.*;
+import java.util.Objects;
+
 @Service
 public class Loader {
-    public Board loadBoard(String file) {
+    public Board loadBoard(String fileName) {
         Board board = new Board();
+        File file = new File(fileName);
+        try {
+            FileInputStream fileInputStream = new FileInputStream(file);
+            InputStreamReader in = new InputStreamReader(fileInputStream);
+            BufferedReader bufferedReader = new BufferedReader(in);
+            String line;
+            int index = 0;
+            while ((line = bufferedReader.readLine()) != null) {
+                for (int i = 0; i < line.length(); i++) {
+                    String symbol = String.valueOf(line.charAt(i));
+                    if (!Objects.equals(symbol, "*")) {
+                        board.setValue(index++, symbol);
+                    }
+                }
+            }
 
+        } catch (IOException e) {
+            //todo need append logger
+        }
 
 
         return board;
