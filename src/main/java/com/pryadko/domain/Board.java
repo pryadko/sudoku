52cad44896c073ee4692c53b589345d3e25f5017
alex <pryadko@gmail.com>
2016-11-01 22:33:47 +0200

    implement toString and create empty board

--- src/main/java/com/pryadko/domain/Board.java
+++ src/main/java/com/pryadko/domain/Board.java
@@ -1,4 +1,29 @@
 package com.pryadko.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
+    private List<Cell> cells = new ArrayList<>(81);
+
+    public Board() {
+        for (int i = 0; i < 81; i++) {
+            cells.add(new Cell(i, 0));
+        }
+    }
+
+    @Override
+    public String toString() {
+        String result = "*************\n";
+        for (int i = 0; i < 9; i++) {
+            result += "*";
+            for (int j = 0; j < 9; j++) {
+                Cell cell = cells.get(i * 9 + j);
+                result += cell.valueToString() + ((j+1)%3==0?"*":"");
+            }
+            result += "\n" +((i+1)%3==0?"*************\n":"");
+
+        }
+        return result;
+    }
 }
