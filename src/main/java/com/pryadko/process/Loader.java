2883048fc84fb99ede7f04c7454afb423ececb67
alex <pryadko@gmail.com>
2016-11-01 23:38:06 +0200

    use Equals instead of ==

---
+++ src/main/java/com/pryadko/process/Loader.java
@@ -0,0 +1,15 @@
+package com.pryadko.process;
+
+import com.pryadko.domain.Board;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Loader {
+    public Board loadBoard(String file) {
+        Board board = new Board();
+
+
+
+        return board;
+    }
+}
