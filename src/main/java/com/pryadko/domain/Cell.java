ed9cc76fe235595f00301b729bcbae79c14212f4
alex <pryadko@gmail.com>
2016-11-01 23:30:28 +0200

    add some method to Cell class

--- src/main/java/com/pryadko/domain/Cell.java
+++ src/main/java/com/pryadko/domain/Cell.java
@@ -1,4 +1,49 @@
 package com.pryadko.domain;
 
 public class Cell {
+    private int id;
+    private int value;
+
+    public Cell(int id, int value) {
+        this.id = id;
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public String valueToString() {
+        return value == 0 ? " " : String.valueOf(value);
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void setValue(String value) {
+        if (value == " ") {
+            this.value = 0;
+        } else {
+            setValue(Integer.parseInt(value));
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cell cell = (Cell) o;
+
+        if (id != cell.id) return false;
+        return value == cell.value;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + value;
+        return result;
+    }
 }
