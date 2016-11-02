a01d663ad139ee9414f5d14211af853681e707a2
alex <pryadko@gmail.com>
2016-11-02 18:25:48 +0200

    add TDD for method getDependentCells

--- src/test/java/com/pryadko/domain/BoardTest.java
+++ src/test/java/com/pryadko/domain/BoardTest.java
@@ -3,6 +3,8 @@ package com.pryadko.domain;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Set;
+
 public class BoardTest {
     @Test
     public void shouldGenereteEmptyBoardWhenWeCreateNewOne() throws Exception {
@@ -10,18 +12,34 @@ public class BoardTest {
 
         Assert.assertEquals(
                 "*************\n" +
-                "*   *   *   *\n" +
-                "*   *   *   *\n" +
-                "*   *   *   *\n" +
-                "*************\n" +
-                "*   *   *   *\n" +
-                "*   *   *   *\n" +
-                "*   *   *   *\n" +
-                "*************\n" +
-                "*   *   *   *\n" +
-                "*   *   *   *\n" +
-                "*   *   *   *\n" +
-                "*************\n",
+                        "*   *   *   *\n" +
+                        "*   *   *   *\n" +
+                        "*   *   *   *\n" +
+                        "*************\n" +
+                        "*   *   *   *\n" +
+                        "*   *   *   *\n" +
+                        "*   *   *   *\n" +
+                        "*************\n" +
+                        "*   *   *   *\n" +
+                        "*   *   *   *\n" +
+                        "*   *   *   *\n" +
+                        "*************\n",
                 board.toString());
+    }
+
+    @Test
+    public void name() throws Exception {
+        Board board = new Board();
+
+        Set<Cell> actual = board.getDependentCell(0);
+
+        Integer[] expected = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 18, 19, 20, 27, 36, 45, 54, 63, 72};
+        Assert.assertArrayEquals(expected, toIds(actual));
+    }
+
+    private Integer[] toIds(Set<Cell> actual) {
+        return actual.stream()
+                .map(Cell::getId)
+                .toArray(Integer[]::new);
     }
 }
