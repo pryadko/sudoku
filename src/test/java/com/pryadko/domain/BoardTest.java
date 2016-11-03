707a696d6b1f36921a5672580859ad82e56c46dd
alex <pryadko@gmail.com>
2016-11-03 22:30:10 +0200

    refactor test and add a few new

--- src/test/java/com/pryadko/domain/BoardTest.java
+++ src/test/java/com/pryadko/domain/BoardTest.java
@@ -5,17 +5,25 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 import static java.util.Comparator.comparingInt;
 
 public class BoardTest {
 
-    private static final Integer[] EXPECTED_FOR_0 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 18, 19, 20, 27, 36, 45, 54, 63, 72};
+    private static final Map<Integer, Integer[]> EXPECTED = new HashMap<Integer, Integer[]>() {
+        {
+            put(0, new Integer[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 18, 19, 20, 27, 36, 45, 54, 63, 72});
+            put(80, new Integer[]{72, 73, 74, 75, 76, 77, 78, 79, 80, 8, 17, 26, 35, 44, 53, 62, 71, 60, 61, 69, 70});
+            put(8, new Integer[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 17, 26, 35, 44, 53, 62, 71, 80, 15, 16, 24, 25});
+        }
+    };
 
     @Before
     public void setUp() throws Exception {
-        Arrays.sort(EXPECTED_FOR_0);
+        EXPECTED.forEach((integer, integers) -> Arrays.sort(integers));
     }
 
     @Test
@@ -45,7 +53,25 @@ public class BoardTest {
 
         Set<Cell> actual = board.getDependentCell(0);
 
-        Assert.assertArrayEquals(EXPECTED_FOR_0, toIds(actual));
+        Assert.assertArrayEquals(EXPECTED.get(0), toIds(actual));
+    }
+
+    @Test
+    public void shouldPopulateDependentsIdForCell80() throws Exception {
+        Board board = new Board();
+
+        Set<Cell> actual = board.getDependentCell(80);
+
+        Assert.assertArrayEquals(EXPECTED.get(80), toIds(actual));
+    }
+
+    @Test
+    public void shouldPopulateDependentsIdForCell8() throws Exception {
+        Board board = new Board();
+
+        Set<Cell> actual = board.getDependentCell(8);
+
+        Assert.assertArrayEquals(EXPECTED.get(8), toIds(actual));
     }
 
     private Integer[] toIds(Set<Cell> actual) {
