bab31cca10c1c7ebaed95eee2db3eb571b25781d
alex <pryadko@gmail.com>
2016-11-04 09:05:15 +0200

    add test to solve easy level

--- src/test/java/com/pryadko/process/LoaderTest.java
+++ src/test/java/com/pryadko/process/LoaderTest.java
@@ -6,25 +6,33 @@ import org.junit.Test;
 
 public class LoaderTest {
 
+    public static final String EXPECTED = "*************\n" +
+            "*534*678*912*\n" +
+            "*672*195*348*\n" +
+            "*198*342*567*\n" +
+            "*************\n" +
+            "*859*761*423*\n" +
+            "*426*853*791*\n" +
+            "*713*924*856*\n" +
+            "*************\n" +
+            "*961*537*284*\n" +
+            "*287*419*635*\n" +
+            "*345*286*179*\n" +
+            "*************\n";
+
     @Test
     public void shouldLoadBoardFromFole() throws Exception {
         Loader loader = new Loader();
         Board board = loader.loadBoard("src/test/resources/input.txt");
 
-        Assert.assertEquals(
-                "*************\n" +
-                        "*534*678*912*\n" +
-                        "*672*195*348*\n" +
-                        "*198*342*567*\n" +
-                        "*************\n" +
-                        "*859*761*423*\n" +
-                        "*426*853*791*\n" +
-                        "*713*924*856*\n" +
-                        "*************\n" +
-                        "*961*537*284*\n" +
-                        "*287*419*635*\n" +
-                        "*345*286*179*\n" +
-                        "*************\n",
-                board.toString());
+        Assert.assertEquals(EXPECTED, board.toString());
+    }
+
+    @Test
+    public void shouldSolveLevel0() throws Exception {
+        Loader loader = new Loader();
+        Board board = loader.loadBoard("src/test/resources/input_level_0.txt");
+
+        Assert.assertEquals(EXPECTED, board.toString());
     }
 }
